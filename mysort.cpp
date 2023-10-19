cd55a9ef7924c5281fc7235012a79d148507aee0
jimbucktwo <92623786+jimbucktwo@users.noreply.github.com>
2023-10-18 17:49:53 -0700

    Added generate mysort and sortrace files

---
+++ mysort.cpp
@@ -0,0 +1,78 @@
+//Jimmy Tran
+// Class (CECS 325-02)
+// Prog 4 Sortrace
+// 10/24/23
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    
+    //prints arguments used for mysort
+    for (int i=0; i<argc; i++){
+        cout << "argv["<<i<<"]: "<<argv[i] << endl;
+    }
+
+    //creates new file fin to be read
+    ifstream fin;
+    fin.open(argv[1], ios::in);
+    
+    // dynamically allocates memory for the array
+    int* numArray = nullptr;
+    int capacity = 10;
+    int size = 0;
+    int num;
+
+    numArray = new int[capacity];
+
+    // creates the array using the file numbers.dat
+    while (fin >> num) {
+        if (size == capacity) {
+            capacity *= 2;
+            int* tempArray = new int[capacity];
+
+            for (int i = 0; i < size; ++ i) {
+                tempArray[i] = numArray[i];
+            }
+
+            delete[] numArray;
+            numArray = tempArray;
+        }
+        numArray[size] = num;
+        ++size;
+    }
+
+    fin.close();
+
+    //bubblesort algorithm
+    for (int i = 0; i < size; ++i) {
+        bool swapped = false;
+
+        for (int j = 0; j < size - 1; ++j) {
+            if (numArray[j] > numArray[j + 1]) {
+                swap(numArray[j], numArray[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) {
+            break;
+        }
+    }
+
+    //creates file mysort.out to write the results into
+    ofstream fout;
+    fout.open(argv[2], ios::out);
+    for (int i = 0; i < size; ++i) {
+        fout << numArray[i] << endl;
+    }
+
+    fout.close();
+    delete[] numArray;
+    
+
+}
---
+++ sortrace.sh
@@ -0,0 +1,21 @@
+#Jimmy Tran
+# Class (CECS 325-02)
+# Prog 4 sortrace
+# 10/24/23
+#
+# I certify that this program is my own original work. I did not copy any part of this program from
+# any other source. I further certify that I typed each and every line of code in this program.
+
+echo Generating 1000000 random numbers
+sleep 1
+./generate 1000000 100000 999999  
+sleep 1
+echo Starting system sort
+sleep 1
+{ time sort numbers.dat > systemsort.out; } 2>&1>> sortrace.log     
+sleep 1
+echo Starting my sort
+sleep 1
+{ time ./mysort numbers.dat mysort.out; } 2>> sortrace.log    
+sleep 1                                          
+sort -c mysort.out 2>> sortrace.log  		            
